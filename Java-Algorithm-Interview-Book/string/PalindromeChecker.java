14c03018a01a38884974be1d7e1436089c841145
na <ghshoot@naver.com>
2026-07-03 20:35:04 +0900

    fix: 정확한 조건 해석 및 다양한 풀이법

--- Java-Algorithm-Interview-Book/string/PalindromeChecker.java
+++ Java-Algorithm-Interview-Book/string/PalindromeChecker.java
@@ -1,13 +1,31 @@
 package string;
 import java.util.Scanner;
 public class PalindromeChecker {
+    public static boolean isPalindrome(String s){
+        //문자 단위로 처리, 원시 타입을 사용하여 실행시간이 빠름.
+//        int start = 0, end = s.length()-1;
+//        while(start < end){
+//            if (!Character.isLetterOrDigit(s.charAt(start))) start++;
+//            else if (!Character.isLetterOrDigit(s.charAt(end))) end--;
+//            else {
+//                if (Character.toLowerCase(s.charAt(start)) == Character.toLowerCase(s.charAt(end
+//                ))) {
+//                    start++;
+//                    end--;
+//                }
+//                else return false;
+//            }
+//        }
+//        return true;
+        //문자열 직접 비교 내장 매서드들을 사용하여 실행 시간은 길되, 가독성이 좋음.
+        String s_filtered = s.replaceAll("[^A-Z0-9a-z]","").toLowerCase();
+        String s_reversed = new StringBuilder(s_filtered).reverse().toString();
+        return s_filtered.equals(s_reversed);
+    }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        String s1 = "";
-        for (int i = s.length()-1; i>=0; i--){
-            s1 += s.charAt(i);
-        }
-        if (s.compareToIgnoreCase(s1) == 0) System.out.println("true");
+        String test1 = "ass d";
+        String test2 = "ass a";
+        System.out.println(isPalindrome(test1));
+        System.out.println(isPalindrome(test2));
     }
 }
