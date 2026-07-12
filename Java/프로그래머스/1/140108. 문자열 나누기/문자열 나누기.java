e0b10fa1f2cad4d4830dab7c43dbe258be9f809e
nanana541 <nag871026@gmail.com>
2026-07-13 00:16:21 +0900

    [level 1] Title: 문자열 나누기, Time: 8.22 ms, Memory: 105 MB -BaekjoonHub

--- Java/프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ Java/프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,6 +1,25 @@
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
-        return answer;
+    public int solution(String t) {
+       int answer = 0;
+       boolean sepStr = false;
+       while (t.length() > 1) {
+           int cnt1 = 0, cnt2 = 0;
+           sepStr = false;
+           for (int i = 0; i < t.length(); i++) {
+               if (t.charAt(0) == t.charAt(i)) cnt1++;
+               else cnt2++;
+               if (cnt1 == cnt2) {
+                   t = t.substring(i + 1);
+                   answer++;
+                   sepStr = true;
+                   break;
+               }
+           }
+           if (!sepStr) break;
+       }
+        if (t.length() == 1 || !sepStr){
+            answer++;
+        }
+       return answer;
     }
 }
